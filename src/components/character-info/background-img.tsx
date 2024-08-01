489163732d209658b84f505087d34ed0f8b1c23b
daggax <daggax8235@gmail.com>
2024-08-02 07:15:28 +0900

    refactor: 캐릭터 페이지 background img 분리.

---
+++ src/components/character-info/background-img.tsx
@@ -0,0 +1,9 @@
+export const BackgroundImg = () => {
+  return (
+    <img
+      src={'/bg1.jpg'}
+      alt={'bg1'}
+      className='absolute object-cover object-center w-full h-full brightness-50'
+    />
+  )
+}
